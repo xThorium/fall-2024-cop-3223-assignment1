021523a7ed0db7a2b15cee12b1e6579539fbb383
Timothy <132708043+xThorium@users.noreply.github.com>
2024-09-08 17:25:27 +0000

    distancetesting

--- fracturing.c
+++ fracturing.c
@@ -5,18 +5,18 @@
 #include <stdio.h>
 #include <math.h>
 
-double calculateDistance();
-{
+double calculateDistance() {
+    
     double x1, y1, x2, y2, distance;
 
     // Asks the user for two sets of points
     printf("Input x1: ");
-    scanf("%lf", &x1)''
+    scanf("%lf", &x1);
     printf("Input y1: ");
     scanf("%lf", &y1);
 
     printf("Input x2: ");
-    scanf("%lf", &x2)''
+    scanf("%lf", &x2);
     printf("Input y2: ");
     scanf("%lf", &y2);
 
@@ -24,28 +24,28 @@ double calculateDistance();
 
     // Returns the distance between two points
     return sqrt(distance);
-}
+    }
 
-double calculatePerimeter();
-{}
+//double calculatePerimeter();
+//{}
 
-double calculateArea();
-{}
+//double calculateArea();
+//{}
 
-double calculateWidth();
-{}
+//double calculateWidth();
+//{}
 
-double calculateHeight();
-{}
+//double calculateHeight();
+//{}
 
 
 int main (int argc, char **argv)
 {
     calculateDistance ();
-    calculatePerimeter ();
-    calculateArea ();
-    calculateWidth ();
-    calculateHeight ();
+    //calculatePerimeter ();
+    //calculateArea ();
+    //calculateWidth ();
+    //calculateHeight ();
 
     return 0;
 }
