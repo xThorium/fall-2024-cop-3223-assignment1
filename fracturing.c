3839847123d3f364994831cfb6bd202a48ce4eff
Timothy <132708043+xThorium@users.noreply.github.com>
2024-09-08 18:07:17 +0000

    fulltesting

--- fracturing.c
+++ fracturing.c
@@ -22,7 +22,7 @@ double calculateDistance() {
 
     // Calculates the distance using a formula, which then prints it for the user to read
     distance = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
-    printf("The distance between the two points is: %.4lf", sqrt(distance));
+    printf("The distance between the two points is: %.4lf\n", sqrt(distance));
 
     return distance;
     }
@@ -34,29 +34,58 @@ double calculatePerimeter() {
 
     // Calulcates the permiter, in this case we're assuming it's a perfectly square city
     double perimeter = distance * 4;
-    printf("The perimeter of the city encompassed by your request is: %.4lf", perimeter);
+    printf("The perimeter of the city encompassed by your request is: %.4lf\n", perimeter);
 
     // Returns what the difficulty of this functio was to make between 1 - 5
     return 2.0;
     }
 
-//double calculateArea();
-//{}
+double calculateArea() {
 
-//double calculateWidth();
-//{}
+    // Reuses the calculateDistance function to avoid needed to redo the code
+    double distance = calculateDistance();
 
-//double calculateHeight();
-//{}
+    // Calculates area based on given inputs
+    double area = distance * distance;
+
+    // Outputs the area
+    printf("The area of the city encompassed by your request is: %.4lf\n", area);
+
+    // Return a difficulty rating
+    return 1.0;
+    }
+
+double calculateWidth() {
+
+    // Asks the user for new point inputs, not reusing that from previous functions
+    double distance = calculateDistance();
+
+    // Calculate Width based on the given x coordinates
+    double width = fabs(x2 - x1);
+    printf("The width of the city encompassed by your request is: %.4lf\n", width);
+
+    // Returns a difficulty rating of the calculateWidth function
+    return 3.0
+    }
+
+
+double calculateHeight() {
+
+    double distance = calculateDistance();
+
+    double height = fabs(y2 - y1);
+    printf("The height of the city encompassed by your request is: %.4lf\n", height);
+    }
+
 
 
 int main (int argc, char **argv)
 {
     double distance = calculateDistance ();
     double perimeter = calculatePerimeter ();
-    //calculateArea ();
-    //calculateWidth ();
-    //calculateHeight ();
+    double area = calculateArea();
+    double width = calculateWidth ();
+    double height = calculateHeight ();
 
     return 0;
 }
