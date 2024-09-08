fdb1f0004d8b661d0451bc76227cce67d23eb75e
Timothy <132708043+xThorium@users.noreply.github.com>
2024-09-08 17:28:12 +0000

    distancetesting

--- fracturing.c
+++ fracturing.c
@@ -20,10 +20,10 @@ double calculateDistance() {
     printf("Input y2: ");
     scanf("%lf", &y2);
 
-    distance = sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+    distance = sqrt(pow(x2-x1)+pow(y2-y1));
 
     // Returns the distance between two points
-    return distance
+    return distance;
     }
 
 //double calculatePerimeter();
